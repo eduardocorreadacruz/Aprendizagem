a7a1f46f72ca808deb000c31997858da96b20994
eduardocorreadacruz <163864869+eduardocorreadacruz@users.noreply.github.com>
2024-11-06 11:19:52 -0300

    sixth-commit

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ function nextSlide() {
 
 setInterval(nextSlide, 3000);
 
-        document.getElementById('linkButton').addEventListener('click', function() {  
-            let linkContainer = document.getElementById('linkContainer');  
-            if (linkContainer.style.display === 'none') {  
-                linkContainer.style.display = 'block';
-            } else {  
-                linkContainer.style.display = 'none';  
-            }  
-        });  
+document.getElementById('linkButton').addEventListener('click', function() {  
+    let linkContainer = document.getElementById('linkContainer');  
+    if (linkContainer.style.display === 'none') {  
+        linkContainer.style.display = 'block';
+    } else {  
+        linkContainer.style.display = 'none';  
+    }  
+});  
 
 function toggleMenu() {  
     let menu = document.getElementById('menuLateral');  
@@ -39,3 +39,29 @@ function toggleMenu() {
         content.style.marginLeft = "0";  
     }  
 }  
+
+document.querySelectorAll('.ancor').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function openCity(evt, cityName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  document.getElementById("defaultOpen").click();
